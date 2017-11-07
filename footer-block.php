db58daf1d93117677be5dfc6a4fdeb39a9657bae
Caelan <caelan.borowiec@pqi.org>
2017-11-07 10:50:57 -0500

    Look up UID by supercookie, and perform login

--- footer-block.php
+++ footer-block.php
@@ -1,14 +1,6 @@
 <?php
+global $user;
 $supercookie = supercookie_instance();
-
-if ($supercookie->uid)
-{
-  $user = user_load($supercookie->uid);
-
-  $form_state = array();
-  $form_state['uid'] = $supercookie->uid;
-  user_login_submit(array(), $form_state);
-}
 
 if ($supercookie->data && $supercookie->uid) // Only save if both IDs exist
 {
@@ -16,5 +8,22 @@ if ($supercookie->data && $supercookie->uid) // Only save if both IDs exist
     ':scid' => $supercookie->data,
     ':uid' => $supercookie->uid
   ));
+}
+
+if (!$user->uid && $supercookie->data)  //Not logged in, but supercookie detected
+{
+  $result = db_query("SELECT `uid` FROM supercookie_sessions WHERE `scid` = :scid LIMIT 1", array(
+    ':scid' => $supercookie->data,
+  ));
+  $records = $result->fetchAll();
+
+  foreach ($records as $record)
+  {
+    $user = user_load($record->uid);
+    //echo 'found user ' .$record->uid;
+    $form_state = array("uid" => $record->uid);
+    $form_state['uid'] = $record->uid;
+    user_login_submit(array(), $form_state);
+  }
 }
 ?>
